06bc64a09d4870479d9039006cf1dca7e4743acb
Yash Parmar <yash20545@gmail.com>
2024-12-19 23:44:39 +0530

    added all header and footer

--- script/index.js
+++ script/index.js
@@ -27,3 +27,19 @@ document.querySelectorAll('.product-hovering').forEach((item) => {
       hoverContainer.style.display = 'none';
   });
 });
+
+
+const searchButton = document.getElementById('searchButton');
+const searchPage = document.getElementById('searchPage');
+const closeSearchPage = document.getElementById('closeSearchPage');
+
+searchPage.style.display = 'none';
+
+searchButton.addEventListener('click', () => {
+  searchPage.style.display = 'block';
+});
+
+
+closeSearchPage.addEventListener('click', () => {
+  searchPage.style.display = 'none'; 
+});
